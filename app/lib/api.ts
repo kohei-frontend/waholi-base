c5af228175e943fed29ca73900140ce9a9112068
Kohei <kohei_shudo@hotmail.com>
2025-01-09 23:08:15 +1100

    fix: ヘッダーにキー追加

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,11 +1,16 @@
 export async function fetchPosts() {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    const apiKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
     if (!apiUrl) {
       console.error("API URLが設定されていません");
       return [];
     }
     try {
-      const res = await fetch(`${apiUrl}post`);
+      const res = await fetch(`${apiUrl}post`, {
+        headers: {
+          'apikey': apiKey, // APIキーをヘッダーに追加
+        }
+      });
       if (!res.ok) {
         throw new Error(`HTTPエラー! ステータス: ${res.status}`);
       }
