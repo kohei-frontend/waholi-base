1e698073669c2968ca80ec9b04aab3900eb662bc
Kohei <kohei_shudo@hotmail.com>
2025-02-05 13:19:28 +1100

    fix: スキーマ変更により型修正

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,18 +1,54 @@
-export type Post = {
+export type Locations = Location[];
+
+export type Image = {
     id: string;
-    userId: string;
-    postType: string;
-    state: string;
-    lga: string;
-    suburb: string;
-    createdAt: string;
-    updatedAt: string;
-    deletedAt: string | null;
+    post_id: string;
+    url: string;
+    created_at: string;
+    deleted_at: string | null;
 };
 
-export type Posts = Post[];
+export type Workplace = {
+    post_id: string;
+    wage: number;
+    atmosphere: string[];
+    rating: number;
+    comment: string;
+};
+
+export type Accommodation = {
+    post_id: string;
+    rent: number;
+    setup: string[];
+    rating: number;
+    comment: string;
+};
+
+export type PostDetail = {
+    id: string;
+    location_id: string;
+    post_type: string;
+    user_id: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string | null;
+    image: Image[];
+    workplace: Workplace | null;
+    accommodation: Accommodation | null;
+};
+
+export type Location = {
+    id: string;
+    name: string;
+    lga: string;
+    state: string;
+    suburb: string;
+    created_at: string;
+    updated_at: string;
+    posts: PostDetail[];
+};
   
-export type SetPosts = (posts: Post[]) => void;
+export type SetLocations = (locations: Location[]) => void;
 
 export type MapContainerRef = React.RefObject<HTMLDivElement | null>;
 
