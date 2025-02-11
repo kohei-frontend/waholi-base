91dceb32135015f058dcb3771d063e982a357d61
Kohei <kohei_shudo@hotmail.com>
2025-02-11 21:39:00 +1100

    feat: storageローカルホストを許可した

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['127.0.0.1'], // ここに許可するホスト名を追加。storageローカルのホスト追加
+  },
 };
 
 export default nextConfig;
