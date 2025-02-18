f476ee79fc4fa50ba6af849330431dd6dee37965
Kohei <kohei_shudo@hotmail.com>
2025-02-18 23:42:31 +1100

    fix: fetchFacilitiesの関数修正

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,22 +1,39 @@
-import { Facility } from "../types";
-
-export async function fetchFacilities(type?: string, wage?: string, rent?: string, rating?: string) {
+export const fetchFacilities = async (params?: { type?: string, wageRange?: [number, number], rating?: number, rentRange?: [number, number] }) => {
   try {
     const queryParams = new URLSearchParams();
-    if (type) queryParams.append('type', type);
-    if (wage) queryParams.append('wage', wage);
-    if (rent) queryParams.append('rent', rent);
-    if (rating) queryParams.append('rating', rating);
+
+    switch (params?.type) {
+      case 'WORKPLACE':
+        queryParams.append('type', params.type.toString());
+        if (params.rating !== undefined && params.rating !== 0) {
+          queryParams.append('rating', params.rating.toString());
+        }
+        if (params.wageRange) {
+          queryParams.append('wageMin', params.wageRange[0].toString());
+          queryParams.append('wageMax', params.wageRange[1].toString());
+        }
+        break;
+      case 'ACCOMMODATION':
+        queryParams.append('type', params.type.toString());
+        if (params.rating !== undefined && params.rating !== 0) {
+          queryParams.append('rating', params.rating.toString());
+        }
+        if (params.rentRange) {
+          queryParams.append('rentMin', params.rentRange[0].toString());
+          queryParams.append('rentMax', params.rentRange[1].toString());
+        }
+        break;
+      default:
+        break;
+    }
 
     const response = await fetch(`/api/facilities?${queryParams.toString()}`);
     if (!response.ok) {
       throw new Error('データの取得に失敗しました');
     }
-    const data = await response.json() as Facility[]; 
-    console.log("results", data);
-    return data;
+    return await response.json();
   } catch (error) {
-    console.error("データの取得中にエラーが発生しました:", error);
-    return [];
+    console.error('データの取得中にエラーが発生しました:', error);
+    throw error;
   }
-}
+};
