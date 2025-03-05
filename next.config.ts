884947030c1763867357f1f32294be4a5fef77c1
Kohei <kohei_shudo@hotmail.com>
2025-03-06 09:46:05 +1100

    fix: supabase(storage)のホスト名を追加

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["127.0.0.1"], // ここに許可するホスト名を追加。storageローカルのホスト追加
+		domains: ["127.0.0.1", "iasttwggkzozjnbdyfxh.supabase.co"],
 	},
 };
 
