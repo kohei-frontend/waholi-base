f9849608bffa36c8b5f008a5e94dc1ac1046ce6f
Kohei <kohei_shudo@hotmail.com>
2025-03-07 23:19:58 +1100

    feat: ホーム画面作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@mantine/core";
+import HomeFeature from "./_features/home";
 
 // メインカラー：#FFFFFF
 // サブカラー：​#A8D5BA
@@ -6,10 +7,8 @@ import { Button } from "@mantine/core";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<Button variant="filed">ボタンだよ</Button>
-			</main>
-		</div>
+		<main className="min-h-screen">
+			<HomeFeature />
+		</main>
 	);
 }
